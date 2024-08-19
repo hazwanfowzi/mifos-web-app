e953ceee9dfcb5f9d8d313f30afe95832dfc59a0
Sallehuddin Abdul Latif <sallehuddin@gmail.com>
2024-08-19 16:12:09 +0800

    unforced npm audit fix

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '240721',
-    'hash': 'd39a6d8a'
+    'version': '240819',
+    'hash': '521fc015'
   },
   'allow_switching_backend_instance': true
 };
